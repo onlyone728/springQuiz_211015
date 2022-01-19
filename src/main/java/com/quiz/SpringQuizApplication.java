6ed68121022853a5d1bd6c140f82602a37812480
장하나 <onlyone728@gmail.com>
2022-01-19 18:55:00 +0900

    설정 수정

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -1,18 +1,10 @@
 package com.quiz;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@MapperScan(basePackages = "com.quiz.*")  // mapper scan 추가!!!
+
 @SpringBootApplication
 public class SpringQuizApplication {
 
@@ -20,14 +12,4 @@ public class SpringQuizApplication {
 		SpringApplication.run(SpringQuizApplication.class, args);
 	}
 
-	@Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-
-        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-        sessionFactory.setMapperLocations(res);
-
-        return sessionFactory.getObject();
-    }
 }
