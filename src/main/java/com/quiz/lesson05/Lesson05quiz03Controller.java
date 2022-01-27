62448f35051221bb0141026799c9a7309131b051
장하나 <onlyone728@gmail.com>
2022-01-27 18:26:03 +0900

    JSTL fmt 과제

---
+++ src/main/java/com/quiz/lesson05/Lesson05quiz03Controller.java
@@ -0,0 +1,51 @@
+package com.quiz.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Lesson05quiz03Controller {
+
+	// http://localhost/lesson05/quiz03
+	@RequestMapping("/lesson05/quiz03")
+	public String quiz03(Model model) {
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057); 
+		
+		List<Map<String, Object>> cardBills = new ArrayList<>();
+
+		Map<String, Object> cardBill = new HashMap<>();
+		cardBill.put("store", "GS48");
+		cardBill.put("pay", 7800);
+		cardBill.put("date", "2025-09-14");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "현태백화점");
+		cardBill.put("pay", 2750000);
+		cardBill.put("date", "2025-09-18");
+		cardBill.put("installment", "3개월");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "요촌치킨");
+		cardBill.put("pay", 180000);
+		cardBill.put("date", "2025-09-20");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+		
+		model.addAttribute("candidates", candidates);
+		model.addAttribute("cardBills", cardBills);
+		
+		return "lesson05/quiz03";
+	}
+}
